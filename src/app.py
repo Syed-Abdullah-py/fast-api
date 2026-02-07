b93f6bd42b66d8232db12afcdc3b2c2fa4fda2f0
Syed Abdullah <smabdullah4945@gmail.com>
2026-02-07 16:30:05 +0500

    Added functionality to upload the image to blob url

--- src/app.py
+++ src/app.py
@@ -4,6 +4,11 @@ from src.schemas import PostCreate, PostResponse
 from src.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from src.images import imagekit
+import shutil
+import os
+import uuid
+import tempfile
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,28 +19,51 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/upload")
 async def upload_file(
-        file: UploadFile = File(...),
-        caption: str = Form(...),
-        session: AsyncSession = Depends(get_async_session)
+    file: UploadFile = File(...),
+    caption: str = Form(...),
+    session: AsyncSession = Depends(get_async_session),
 ):
-    post = Post(
-        caption=caption,
-        url="dummy.url",
-        file_type="photo",
-        file_name="dummy name",
-    )
+    temp_file_path = None
 
-    # Staging
-    session.add(post)
+    try:
+        # Extract extension correctly
+        _, ext = os.path.splitext(file.filename)
 
-    # Actually adding
-    await session.commit()
+        # Save temp file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
 
-    # Hydrating the default fields
-    await session.refresh(post)
+        # Upload to ImageKit
+        with open(temp_file_path, "rb") as f:
+            upload_result = imagekit.files.upload(
+                file=f,
+                file_name=file.filename,
+                tags=["backend-upload"],
+            )
 
-    # Return the Hydrated post object
-    return post
+        # If we are here, upload succeeded
+        post = Post(
+            caption=caption,
+            url=upload_result.url,
+            file_type=upload_result.file_type,
+            file_name=upload_result.name,
+        )
+
+        session.add(post)
+        await session.commit()
+        await session.refresh(post)
+
+        return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
+
+        await file.close()
 
 @app.get("/feed")
 async def get_feed(
